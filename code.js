6e5f94db81dfd303395bc519a9976fb219c5f647
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-04-17 23:41:35 -0600

    Update code.js
    what I have so far

--- code.js
+++ code.js
@@ -1,8 +1,35 @@
-function mergesort(array) {
-    let h = 0;
-    for(let j = array.length; j > 1; j = Math.ceil(j/2)){
-        for(let i = 0; i < Math.ceil(Math.log2(array.length)); i++){
+//I think I need to swap the inner loop with the outer loop
+// function mergesort(array) {
+//     let h = Math.ceil(Math.log2(array.length));
+//     for(let j = array.length; j > 1; j = Math.ceil(j/2)){
+//         for(let i = 0; i < h; i++){
 
+//         }
+//         h--;
+//     }
+// }
+
+function mergesort(array){
+    let h = Math.ceil(Math.log2(array.length));
+    for(let i = 0; i < h; i++){
+        console.log(i);
+        for(let j = array.length; j > 0; j -= 2){
+            // console.log(j);
+            if(array[j - 1] < array[j - 2]){
+                swap(array, j - 1, j - 2);
+            }
         }
+        // h--;
     }
+    return array;
 }
+
+function swap(arr, index1, index2){
+    let tmp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = tmp;
+    return arr;
+}
+
+let array = [8,2,4,5,7,1,6,3];
+console.log(mergesort(array));
