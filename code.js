589056791a467238cda54b76d1a24a6b5a2a86cb
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-04-17 21:34:28 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,8 @@
 function mergesort(array) {
-    return array;
+    let h = 0;
+    for(let j = array.length; j > 1; j = Math.ceil(j/2)){
+        for(let i = 0; i < Math.ceil(Math.log2(array.length)); i++){
+
+        }
+    }
 }
