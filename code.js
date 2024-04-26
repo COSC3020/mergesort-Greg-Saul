40a80ba8f886f4cad135369ab33bb9917d3f1a61
Greg-Saul <123204473+Greg-Saul@users.noreply.github.com>
2024-04-26 10:12:22 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,51 +1,60 @@
-//This code is heavily influenced by https://github.com/COSC3020/mergesort-ZachRenz/blob/main/code.js 
-//especially the nested for loops and for other cases where I got very stuck
-
 function mergesort(array) {
     if(array.length < 2){
         return array;
     }
-
-    for(i = 0, j = 1, sizeToMerge = 2; sizeToMerge < 2 * array.length; i = 0, j = sizeToMerge, sizeToMerge *= 2){
-        for(; j < array.length; i += sizeToMerge, j += sizeToMerge){
-            merge(sizeToMerge, i, j, array);
+    // console.log(array.length)
+    for(let size = 1; size < array.length; size *= 2){ //size is the size of the "sub arrays"
+        //  console.log("size: " + size)
+        for(let start = 0; start < array.length; start += size * 2){
+            // console.log(start)
+            let mid = Math.min(start + size - 1, array.length - 1);
+            var end = Math.min(start + 2 * size - 1, array.length - 1);
+            merge(array, start, mid, end);
         }
     }
-
     return array;
 }
 
-function merge(size, i, j, arr){
-    let merged = false;
-    while(merged == false){
-        if(i == j)
-            merged = true;
-        else if(arr[i] <= arr[j])
-            i++;
-        else if(arr[i]>arr[j]){
-            let k = j + 1;
-            while(arr[k] < arr[i] && k <= (j - 1 + size)){
-                k++;
+function merge(arr, s, m, e){
+    let s2 = m + 1; // start of array 2
+ 
+    if (arr[m] <= arr[s2]) // already sorted for the first 2
+    {
+        return;
+    }
+ 
+    while (s <= m && s2 <= e)
+    {
+        if (arr[s] <= arr[s2])
+        {
+            s++;
+        }
+        else
+        {
+            let value = arr[s2];
+            let index = s2;
+            while (index != s) 
+            {
+                arr[index] = arr[index - 1];
+                index--;
             }
-            k--;
-            swap(arr,i,k);
+            arr[s] = value;
+            s++;
+            m++;
+            s2++;
         }
     }
 }
 
-function swap(a,i,k){
-    let tmp = a[k];
-    a[k] = a[i];
-    a[i] = tmp;
-}
 
-
-//   let arr = [8,73,2,4,0,5,987,2,709,8,4758,9,768,95,79,8,27,4];
+  let arr = [8,73,2,4,0,5,987,2,709,8,4758,9,768,95,79,8,27,4];
 //   console.log(arr);
-//   mergesort(arr);
+  console.log(mergesort(arr));
 //   console.log(arr);
 
-// This was my attempt to get the correct amount of iterations. I think that my biggest issue is that the loops are nested in the wrong order. These never worked out for me so I had to look to zach for help
+
+
+// This was my attempt to get the correct amount of iterations. I think that my biggest issue is that the loops are nested in the wrong order. These never worked out for me so I had to look online
 
 // function mergesort(array) {
 //     return array;
